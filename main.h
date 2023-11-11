badecd133fc503b5a559061a42f0760245fd489e
Zakaria AIT ALI <zakaria.aitali@etu.uae.ac.ma>
2023-11-11 16:24:59 +0100

    added Prototypes to header

--- main.h
+++ main.h
@@ -1,4 +1,13 @@
 #ifndef PRINTF_MAIN_H
 #define PRINTF_MAIN_H
 
-#endif //PRINTF_MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
+
+/* _putchar.c */
+int _putchar(char c);
+
+/* printf.c */
+int _printf(const char *format, ...);
+
+#endif /* PRINTF_MAIN_H */
